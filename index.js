da25998147e114a4481bf9ab1560062b9b666377
Matheus Quirino <105397038+mthflashice@users.noreply.github.com>
2023-04-08 23:49:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,30 @@
-let firstNumber = prompt('Digite o primeiro número')
-let secondNumber = prompt('Digite o segundo número')
-
-firstNumber = Number(firstNumber)
-secondNumber = Number(secondNumber)
-
-const sum = firstNumber + secondNumber
-const sub = firstNumber - secondNumber
-const multi = firstNumber * secondNumber
-const div = firstNumber / secondNumber
-const restDiv = firstNumber % secondNumber
-
-
-
-alert(`A soma dos dois números é` + sum)
-alert('A Subtração dos números é: ' + sub)
-alert('A Multiplicação dos números é : ' + multi)
-alert('Divisão dos números é: ' + div)
-alert('Resto da divisão: ' + restDiv)
-function ParImpar(sum){
-    if(sum % 2 ==0){
-        (alert `O Número é 
-        par!`)   
-    }else{
-        (alert `O Número é
-        Impar!`)
-    }
-}
-ParImpar(sum);
+students=   
+    [
+        { name: "Matheus Quirino", nota1: 7, nota2: 7 },
+        { name: "Camila", nota1: 4, nota2: 5 },
+        { name: "Melqui", nota1: 2, nota2: 3 },
+        { name: "Mená", nota1: 10, nota2: 9.5 },
+        { name: "Máx", nota1: 5, nota2: 8 }
+      ]
 
 
 
 
+const calcMedia = (nota1,nota2) => {
+    return ((nota1 + nota2) / 2).toFixed(2)
+  }
+ 
+  
 
-if(firstNumber===secondNumber){
-alert(`Os números inseridos são 
-iguais`)
-}
-if(firstNumber!=secondNumber){
-    alert(`Os números inseridos são 
-diferentes`)
-}
+  for (let media of students) 
+  if(calcMedia(media.nota1, media.nota2) >= 7) {
+    alert(`A média do(a) aluno(a) ${media.name} foi : ${calcMedia(media.nota1,media.nota2)}
+    Parabéns!, ${media.name}! Você foi aprovado(a) no curso!`)
+} else if (media.nota1, media.nota2< 7 && media.nota1, media.nota2>=5){
+    alert(`A média do(a) aluno(a) ${media.name} foi: ${calcMedia(media.nota1,media.nota2)}
+    Estude para sua prova de recuperação!, ${media.name}! Boa Sorte!`)
+} else  {
+    alert(`A média do(a) aluno(a) ${media.name} foi: ${calcMedia(media.nota1,media.nota2)}
+    Você não passou dessa vez, ${media.name}! Tente novamente no Próximo Semestre !`)
+   
+    };
